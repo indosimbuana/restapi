577432cb1aaae992efd53ccd66a872d2341c8a9b
Agung Prabowo <agungpasific@gmail.com>
2021-05-03 13:19:02 +0700

    update poli

--- application/models/Mantrian.php
+++ application/models/Mantrian.php
@@ -14,7 +14,7 @@ class Mantrian extends CI_Model
     function poli($tgl)
     {
         $db2 = $this->load->database('antrian', TRUE);
-        $sql = $db2->query("SELECT MAX(NO) as noantri, Klinik as klinik, DOKTER as dokter, Tanggal as tanggal FROM tbantrianpoli WHERE DATE(Tanggal) = '$tgl' AND Masuk = 1 GROUP BY Klinik");
+        $sql = $db2->query("SELECT MAX(NO) as noantri, Klinik as klinik, DOKTER as dokter, Tanggal as tanggal FROM tbantrianpoli WHERE DATE(Tanggal) = '$tgl' GROUP BY Klinik");
         return $sql->result_array();
     }
 
